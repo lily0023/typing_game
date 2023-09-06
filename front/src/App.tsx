92165c703fe2dcd1eeb59800673eb83200ef0e13
hiro97617 <cf24bnva5@gmail.com>
2023-09-06 12:33:16 +0900

    tailwindのインストールが完了しました。スクショを見てください！！

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="underline text-red-400">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
